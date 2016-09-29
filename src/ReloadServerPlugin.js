659151799533c28d1c606804e09f8ab4d87a02de
Ted Piotrowski <tppiotrowski@gmail.com>
2016-09-28 22:35:56 -0400

    add args

--- src/ReloadServerPlugin.js
+++ src/ReloadServerPlugin.js
@@ -3,15 +3,17 @@ import path from "path";
 
 const defaultOptions = {
   script: "server.js",
+  args: []
 };
 
 export default class ReloadServerPlugin {
-  constructor({ script } = defaultOptions) {
+  constructor({ script, args } = defaultOptions) {
     this.done = null;
     this.workers = [];
 
     cluster.setupMaster({
       exec: path.resolve(process.cwd(), script),
+      args: args 
     });
 
     cluster.on("online", (worker) => {
